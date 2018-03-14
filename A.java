0813aaedcad22298324b7727d9b4dc4c9d43e254
sateesh <sateesh.jalda2020@gmail.com>
2018-03-15 01:12:36 +0530

    second commit

---
+++ A.java
@@ -0,0 +1,11 @@
+public class A{
+
+public static void main(String[] args){
+
+Person objperson = new Person();
+
+objperson.setName("jalda sateesh");
+
+System.out.println("Employe Name Is "+objperson.getName());
+}
+}
